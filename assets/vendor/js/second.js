400917662ed44c68c6b06ed5f20bb7e8f5ec4eee
RoyalServiceAgency <leadermelan@gmail.com>
2025-02-22 21:26:27 +0000

    modification royalservice

---
+++ assets/vendor/js/second.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+         $('#myCarouselPartners').carousel({
+           interval: 90, // Changer le temps en millisecondes ici (3000ms = 3 secondes)
+           wrap: true // Permet le défilement en boucle
+         });
+    
+       });
